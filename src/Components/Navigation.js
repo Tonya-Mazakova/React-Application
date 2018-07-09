f369c0b0489b04588710bd3ac1075b67d0c7c810
antanina.mazakova <antanina.mazakova@leverx.com>
2018-07-09 12:10:06 +0300

    comment

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -7,16 +7,16 @@ class Navigation extends PureComponent{
     render(){
         return (<div className='nav-scroller py-1 mb-2'>
                         <nav className="nav d-flex justify-content-between">
-                        <a className="p-2 text-muted" href="#">World</a>
-                        <a className="p-2 text-muted" href="#">Technology</a>
-                        <a className="p-2 text-muted" href="#">Culture</a>
-                        <a className="p-2 text-muted" href="#">Health</a>
-                        <a className="p-2 text-muted" href="#">Buisness</a>
-                        <a className="p-2 text-muted" href="#">Style</a>
-                        <a className="p-2 text-muted" href="#">Opinion</a>
-                        <a className="p-2 text-muted" href="#">Travel</a>
-                        <a className="p-2 text-muted" href="#">Design</a>
-                        <a className="p-2 text-muted" href="#">Music</a>
+                        <a className="p-2" href="#">World</a>
+                        <a className="p-2" href="#">Technology</a>
+                        <a className="p-2" href="#">Culture</a>
+                        <a className="p-2" href="#">Health</a>
+                        <a className="p-2" href="#">Buisness</a>
+                        <a className="p-2" href="#">Style</a>
+                        <a className="p-2" href="#">Opinion</a>
+                        <a className="p-2" href="#">Travel</a>
+                        <a className="p-2" href="#">Design</a>
+                        <a className="p-2" href="#">Music</a>
                     </nav>
                 </div>)
         }
