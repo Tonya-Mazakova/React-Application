838b86be88ffb8b9c1fa46bd67330b14a7fe79d0
antanina.mazakova <antanina.mazakova@leverx.com>
2018-07-10 12:54:14 +0300

    comment

--- src/Components/style/PostView.js
+++ src/Components/style/PostView.js
@@ -6,18 +6,19 @@ const Wrapper = styled.div`
 `;
 
 const PostWrap = styled.div`
-    height: 300px;
+    //height: 300px;
     background-color: azure;
     margin: 25px auto 0;
     border: 10px solid white;
 `;
 
 const TitleWrap = styled.div`
-    height: 80px;
+    //height: 80px;
     border-bottom: 1px solid lightgoldenrodyellow;
     background-color: antiquewhite;
     text-align: center;
     font-size: 20px;
+    padding: 10px;
 `;
 
 const BodyPost = styled.div`
@@ -30,7 +31,7 @@ const BackWrap = styled.a`
 `;
 
 const TitlePost = styled.h5`
-    line-height: 80px;
+    line-height: 25px;
 `;
 
 
